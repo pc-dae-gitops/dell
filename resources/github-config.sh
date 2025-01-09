79752667ccf77849ebcddc43aa0f2a467c2d00b2
Paul Carlton <paul.carlton414@gmail.com>
2025-01-09 15:42:22 +0000

    update config

--- resources/github-config.sh
+++ resources/github-config.sh
@@ -1,3 +1,4 @@
-export GITHUB_AUTH_ORG=pc-dae-gitops
-export GITHUB_HOSTNAME=github.com
-export GIT_HOST_TYPE="github.com=github"
+GITHUB_AUTH_ORG=pc-gitops
+GITHUB_HOSTNAME=github.com
+GIT_HOST_TYPE="github.com=github"
+
--- resources/github-secrets-sample.sh
+++
@@ -1,6 +0,0 @@
-export GITHUB_CLIENT_ID=... # Github Organisation Client ID
-export GITHUB_CLIENT_SECRET=... # Github Organisation Client Secret
-export VAULT_DEX_CLIENT_SECRET="$(date +%s | sha256sum | base64 | head -c 10)"
-
-export GITHUB_TOKEN_WRITE=... # write access token
-export GITHUB_TOKEN_READ=... # read access token
